abfcfed8e6fffccd12f2f3cd9208151eed7d2e05
Miles Benjamin <louren.of.courone@gmail.com>
2018-07-04 16:55:10 -0400

    Adding action model interface

---
+++ Assets/Scripts/DialogueModel/IGameActionModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IGameActionModel {
+
+    void resolve();
+    bool validate();
+	
+}
+
+public class shipDamage : IGameActionModel
+{
+
+    public void resolve()
+    {
+
+    }
+
+    public bool validate()
+    {
+        return true;
+    }
+}
