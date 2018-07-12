e5128233adbb8951bba9e0f53013552336563bcc
Adam Frieden <adam@frieden.org>
2018-07-12 19:29:31 -0400

    project skeleton

--- Assets/Scripts/Helpers/ActiveTextHelpers.cs
+++ Assets/Scripts/Helpers/ActiveTextHelpers.cs
@@ -1,7 +1,4 @@
 ﻿
-using PixelSpace.Models.SharedModels;
-using PixelSpace.Models.SharedModels.Ships;
-
 namespace PixelShips.Helpers
 {
     public static class ActiveTextHelpers
@@ -14,29 +11,6 @@ namespace PixelShips.Helpers
         public static string AddColor(this string text, string color)
         {
             return string.Format("<color={0}>{1}</color>", color, text);
-        }
-
-        public static string GetActiveTextTag(this Ship ship, bool includeId = false)
-        {
-            var includedIdText = includeId ? " " + ship.Id.Substring(0,4) : string.Empty;
-            var shipTag = string.Format("[{0}{1}]", ship.Name, includedIdText);
-            return shipTag.AddLink(ship.Id).AddColor("purple");
-        }
-
-        public static string GetActiveTextTage(this Room room, bool includeId = false)
-        {
-            return string.Empty;
-        }
-
-        public static string GetActiveRoomText(this string text, string roomId)
-        {
-            return text.AddLink(roomId).AddColor("orange");
-        }
-
-        public static string GetActiveRoomText(string roomId)
-        {
-            var t = string.Format("[Sector {0}]", roomId.Substring(0, 4));
-            return t.AddLink(roomId).AddColor("orange");
         }
     }
 }
