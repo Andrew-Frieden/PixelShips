3bac95c644e7072502697c4d49c471605481cbfd
Andrew Frieden <drew@ninjaparty.net>
2018-08-12 16:56:15 -0400

    Entity Classes and meta files

---
+++ Assets/Scripts/Models/PlayerShip.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Models;
+using UnityEngine;
+
+namespace Models
+{
+    public class PlayerShip : Ship
+    {
+        public PlayerShip(int hull, int gathing, int transport, int intelligence, int combat, int speed) : base(hull, gathing, transport, intelligence, combat, speed) { }
+    }
+}
---
+++ Assets/Scripts/Models/Ship.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Models
+{
+    [CreateAssetMenu]
+    public class Ship : ScriptableObject, IShip
+    {
+        public int Hull { get; set; }
+        public int Gathering { get; set; }
+        public int Transport { get; set; }
+        public int Intelligence { get; set; }
+        public int Combat { get; set; }
+        public int Speed { get; set; }
+        
+        public Ship(int hull, int gathing, int transport, int intelligence, int combat, int speed)
+        {
+            Hull = hull;
+            Gathering = gathing;
+            Transport = transport;
+            Intelligence = intelligence;
+            Combat = combat;
+            Speed = speed;
+        }
+    } 
+}
+
