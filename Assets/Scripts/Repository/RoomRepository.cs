54c740e5005bb0e867acb4093bf4fea677fb1638
Miles Benjamin <louren.of.courone@gmail.com>
2018-08-21 16:50:51 -0400

    Cleaned up lead-in text generator

--- Assets/Scripts/Repository/RoomRepository.cs
+++ Assets/Scripts/Repository/RoomRepository.cs
@@ -82,17 +82,23 @@ namespace Repository
 
             };
 
-            return new Room( leadIn + " {{ link }}. " + mainText, roomName, null, new List<IRoomEntity>() { }, roomContent);
+            return new Room( leadIn + mainText, roomName, null, new List<IRoomEntity>() { }, roomContent);
         }
 
         private string randomizeLeadIn()
         {
             var leadInTexts = new List<string>
             {
-                "Your jump leaves you in a",
-                "Your ship arrives in a",
-                "Your sensors indicate that the system you’ve entered is a",
-                "Out the window you see a"
+                "Your jump leaves you in a {{ link }}. ",
+                "Your ship arrives in a {{ link }}. ",
+                "Your sensors indicate that the system you’ve entered is a {{ link }}. ",
+                "Out the window you see a {{ link }}. ",
+                "Your command viewport resolves into a view of a {{ link }}. ",
+                "As your jump drives spin down, you look out to see a {{ link }}. ",
+                "Jump drives still whirring, a {{ link }} fills your screen. ",
+                "As your ship decelerates rapidly, a {{ link }} comes into view. ",
+                "\"Captain!  We've spotted a nearby {{ link }},\" your first mate shouts. ",
+                "\" We have arrived at your destination... a {{ link }}... thank you for flying jump drive spacelines!\" "
 
             };
 
