299bc32e684ef6cc5244b258f92c914e5dc56b44
Adam Frieden <adam@frieden.org>
2018-10-13 12:41:39 -0400

    save and load support for warp actions

--- Assets/Scripts/Models/Actions/WarpAction.cs
+++ Assets/Scripts/Models/Actions/WarpAction.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Models.Factories;
 using Controller;
+using Models.Dtos;
 
 namespace Models.Actions
 {
@@ -10,17 +11,49 @@ namespace Models.Actions
         //  instead of figuring out how to serialize a RoomTemplate as part of a SimpleAction
         //  or build a non-SimpleAction,
         //  just stuff all the RoomTemplate properties 
-        private const string ActorFlavor1 = "ActorFlavor1";
-        private const string ActorFlavor2 = "ActorFlavor2";
-        private const string ActorFlavor3 = "ActorFlavor3";
-        private const string Difficulty = "ActorFlavor3";
-        private const string RoomFlavor = "RoomFlavor";
+        private const string ActorFlavorKey = "ActorFlavor";
+        private const string DifficultyKey = "RoomDifficulty";
+        private const string RoomFlavorKey = "RoomFlavor";
 
-        private readonly RoomTemplate _template;
-        
+        private RoomTemplate _template;
+
+        private void StoreTemplateStats()
+        {
+            Stats[RoomFlavorKey] = (int)_template.Flavor;
+            Stats[DifficultyKey] = (int)_template.Difficulty;
+
+            int flavorCount = 0;
+            foreach (var actorFlavor in _template.ActorFlavors)
+            {
+                var key = $"{ActorFlavorKey}{flavorCount}";
+                Stats[key] = (int)actorFlavor;
+                flavorCount++;
+            }
+        }
+
+        private void SetupTemplateFromStats()
+        {
+            var actorFlavors = new List<RoomActorFlavor>();
+            var flavorCount = 0;
+            var key = $"{ActorFlavorKey}{flavorCount}";
+
+            while (Stats.ContainsKey(key))
+            {
+                actorFlavors.Add((RoomActorFlavor)Stats[key]);
+                flavorCount++;
+                key = $"{ActorFlavorKey}{flavorCount}";
+            }
+        }
+
         public WarpAction(RoomTemplate template)
         {
             _template = template;
+            StoreTemplateStats();
+        }
+
+        public WarpAction(SimpleActionDto dto, IRoom room) : base(dto, room)
+        {
+            SetupTemplateFromStats();
         }
         
         public override IEnumerable<TagString> Execute(IRoom room)
