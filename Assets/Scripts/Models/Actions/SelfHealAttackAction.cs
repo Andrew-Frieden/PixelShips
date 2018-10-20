274863213e732027ec7fedbe6a119189e0a31fe7
Miles Benjamin <louren.of.courone@gmail.com>
2018-10-20 14:06:13 -0400

    Adding Self Heal weapon, not hooked up in game anywhere

---
+++ Assets/Scripts/Models/Actions/SelfHealAttackAction.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using Helpers;
+using Models.Dtos;
+using Models.Stats;
+using TextEncoding;
+
+namespace Models.Actions
+{
+    public class SelfHealAttackAction : SimpleAction
+    {
+        private const string WeaponKey = "weapon";
+        private string Weapon
+        {
+            get
+            {
+                return Values[WeaponKey];
+            }
+            set
+            {
+                Values[WeaponKey] = value;
+            }
+        }
+
+        private int BaseHeal;
+
+        public SelfHealAttackAction(SimpleActionDto dto, IRoom room) : base(dto, room) { }
+
+        public SelfHealAttackAction(IRoomActor source, IRoomActor target, int damage, int heal, string weapon, int energy)
+        {
+            Source = source;
+            Target = target;
+            Stats = new Dictionary<string, int>();
+            BaseDamage = damage;
+            BaseHeal = heal;
+            Weapon = weapon;
+            Energy = energy;
+        }
+
+        public override IEnumerable<TagString> Execute(IRoom room)
+        {
+            base.Execute(room);
+            
+            var actualDamage = Target.TakeDamage(BaseDamage);
+            var actualHeal = Target.HealDamage(BaseHeal);
+
+            Target.IsHostile = true;
+            
+            if (Source is CommandShip)
+            {
+                ActionTags.Add(EventTag.PlayerHealed);
+                return new List<TagString>()
+                {
+                    new TagString()
+                    {
+                        Text = ("< > deal " + actualDamage + " damage and heal " + actualHeal + " with your " + Weapon + ".").Encode(Source.GetLinkText(), Source.Id, LinkColors.Player),
+                        Tags = ActionTags
+                    }
+                };
+            }
+            else
+            {
+                ActionTags.Add(EventTag.PlayerDamaged);
+                return new List<TagString>()
+                {
+
+                    new TagString()
+                    {
+                        Text = ("< > dealt you " + actualDamage + " damage and healed " + actualHeal +  " with it's " + Weapon + ".").Encode(Source.GetLinkText(), Source.Id, LinkColors.HostileEntity),
+                        Tags = ActionTags
+                    }
+                };
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Models/Items/Weapons/SelfHealWeapon.cs
@@ -0,0 +1,44 @@
+﻿using Models;
+using Models.Actions;
+using Models.Dialogue;
+using Models.Dtos;
+using Models.Stats;
+using TextEncoding;
+
+namespace Items
+{
+    public class SelfHealWeapon: Weapon
+    {
+        public SelfHealWeapon(FlexEntityDto dto) : base(dto) { }
+
+        public SelfHealWeapon(FlexData data) : base(data) { }
+        
+        public override IRoomAction MainAction(IRoom room)
+        {
+            return new DoNothingAction(this);
+        }
+
+        public override IRoomAction GetAttackAction(IRoom room, IRoomActor src, IRoomActor target)
+        {
+            return new AttackAction(src, target, BaseDamage, Name, Energy);
+        }
+
+        public override void CalculateDialogue(IRoom room)
+        {
+            switch (CurrentState)
+            {
+                case (int) WeaponState.Unequipped:
+                    DialogueContent = DialogueBuilder.Init()
+                        .AddMainText(Values[ValueKeys.DialogueText].Encode(this, LinkColors.Weapon))
+                        .AddOption("Pickup", new PickupWeaponAction(room.PlayerShip, this))
+                        .Build();
+                    break;
+                case (int) WeaponState.Equipped:
+                    DialogueContent = DialogueBuilder.Init()
+                        .AddMainText($"<>{Env.ll}{Description}{Env.ll}Currently equipped to your ship.".Encode(this, LinkColors.Weapon))
+                        .Build();
+                    break;
+            }
+        }
+    }
+}
