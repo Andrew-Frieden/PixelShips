47bcda312480e19bc13c01526d016ab1c558eade
Andrew Frieden <drew@ninjaparty.net>
2018-11-04 09:11:44 -0500

    New dev tool - room generator (start)

---
+++ Assets/Scripts/Controllers/RoomGeneratorController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using TextSpace.Framework.IoC;
+using TextSpace.Models;
+using TextSpace.Services.Factories;
+using TMPro;
+using UnityEngine;
+
+namespace TextSpace.Controllers
+{
+	public class RoomGeneratorController : MonoBehaviour
+	{
+		[SerializeField] private TMP_Dropdown _flavorFilterDropdown;
+		[SerializeField] private TMP_Dropdown _contentTypeFilterDropdown;
+		[SerializeField] private TMP_Dropdown _entityChooserDropdown;
+		
+		private RoomFactoryService RoomFactory => ServiceContainer.Resolve<RoomFactoryService>();
+		
+		void Start()
+		{
+			var flavorFilterOptions = new List<TMP_Dropdown.OptionData>();
+			var contentTypeFilterOptions = new List<TMP_Dropdown.OptionData>();
+
+			foreach (var flavor in Enum.GetValues(typeof(RoomFlavor)))
+			{
+				flavorFilterOptions.Add(new TMP_Dropdown.OptionData()
+				{
+					text = flavor.ToString()
+				});
+			}
+
+			foreach (var flavor in Enum.GetValues(typeof(RoomActorFlavor)))
+			{
+				contentTypeFilterOptions.Add(new TMP_Dropdown.OptionData()
+				{
+					text = flavor.ToString()
+				});
+			}
+			
+			_flavorFilterDropdown.options = flavorFilterOptions;
+			_contentTypeFilterDropdown.options = contentTypeFilterOptions;
+			
+			//_flavorFilterDropdown.onValueChanged
+		}
+	}
+}
